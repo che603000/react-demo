bc14a5f373bf59e569689c45b4d915b51991d2c9
Alex <che603000@gmail.com>
2015-11-12 19:59:59 +0300

    bootstrap-react#2

--- client/js/components/tabs.jsx
+++ client/js/components/tabs.jsx
@@ -1,36 +1,149 @@
 /**
  * Created by alex on 11.11.2015.
  */
-import {Tabs, Tab, Nav, MenuItem} from "react-bootstrap"
+//import {Tabs, Tab, Nav, MenuItem} from "react-bootstrap"
 
 // components
 import LoginForm  from './form';
 import TabDropdown  from './tab-dropdown';
 
-export default class ControlledTabs extends React.Component {
+
+class Pane extends React.Component {
 
     state = {
-        key: 2
+        isActive: !!this.props.active
     }
 
     constructor(props) {
         super(props);
-        this.handleSelect = this.handleSelect.bind(this);
+
     }
 
-    handleSelect(key) {
-        //alert('selected ' + key);
-        this.setState({key});
+    setSelect(value) {
+        this.setState({isActive: value});
+    }
+
+    render() {
+
+        var _className = this.state.isActive ? "tab-pane active" : 'tab-pane';
+        return (
+            <div className={_className}>{this.props.context}</div>
+        )
+
+    }
+}
+
+class Tab extends React.Component {
+
+    state = {
+        isActive: !!this.props.active
+    }
+
+    constructor(props) {
+        super(props);
+        this.onSelect = this.onSelect.bind(this);
+    }
+
+    setSelect(value) {
+        this.setState({isActive: value});
+    }
+
+    onSelect(e) {
+        //this.setSelect(true);
+        this.props.onSelect && this.props.onSelect(this);
+        console.log(e);
+    }
+
+    render() {
+
+        var _className = this.state.isActive ? "active" : '';
+        return (
+            <li className={_className}>
+                <a href={"#"+this.props.title} onClick={this.onSelect}>{this.props.title}</a>
+            </li>
+        )
+
+
+    }
+}
+
+class Tabs extends React.Component {
+
+    state = {
+        activeTab: !!this.props.active
+    }
+
+    constructor(props) {
+        super(props);
+        this.onSelect = this.onSelect.bind(this);
+    }
+
+    setSelect(value) {
+        this.setState({isActive: value});
+    }
+
+    onSelect(tab) {
+        //this.setSelect(true);
+        this.setState({activeTab: tab});
+        for (let p in this.refs)
+            this.refs[p].setSelect(this.refs[p] === tab);
+
+        this.props.onSelect && this.props.onSelect(this.state.activeTab);
+        //console.log(e);
+    }
+
+    render() {
+        let _className = this.state.isActive ? "active" : '',
+            tabs = this.props.children.map((tab, index)=> {
+                let text = '_tab' + index;
+                return React.cloneElement(tab, {
+                    ref: text,
+                    key: index,
+                    title: tab.props.title || text,
+                    onSelect: this.onSelect
+                });
+            });
+
+
+        return (
+            <div>
+                <ul className="nav nav-tabs">{tabs}</ul>
+                <div className="tab-content">
+                    {
+                        tabs.map((t,i)=>{
+                            return(
+                            <Pane key={i} ref={"pane"+i} active={t.props.active} context={t.props.children}/>
+                                )
+                            })
+                        }
+                </div>
+            </div>
+        )
+
+    }
+}
+
+
+export default class ControlledTabs extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.onSelect = this.onSelect.bind(this);
+    }
+
+    onSelect(tab) {
+        this.setState({activeTab: tab});
+        for (let t in this.refs)
+            this.refs[t].setSelect(this.refs[t] === tab);
     }
 
     render() {
         return (
-            <Tabs activeKey={this.state.key} onSelect={this.handleSelect}>
-                <Tab eventKey={1} title="Tab 1"><LoginForm model={this.props.model}/></Tab>
-                <Tab eventKey={2} title="Tab 2">Tab 2 content</Tab>
-                <Tab eventKey={3} title="Tab 3" disabled>Tab 3 content</Tab>
-
-                <TabDropdown eventKey={4} title="Tab 4">Tab 4</TabDropdown>
+            <Tabs>
+                <Tab title="TAB 1111" active>
+                    <LoginForm model={this.props.model}/>
+                </Tab>
+                <Tab title="TAB 2222"/>
             </Tabs>
         );
     }
